f294fd618910ec90e8784eef52bc1d40c0a69d88
Meghan WESTVIG <mwestvig@c2r6s5.wethinkcode.co.za>
2019-06-04 13:39:43 +0200

    Adding the skeletons for d00/ex01 files.

---
+++ d00/ex01/addContact.cpp
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   addContact.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwestvig <mwestvig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 13:20:40 by mwestvig          #+#    #+#             */
+/*   Updated: 2019/06/04 13:39:29 by mwestvig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/*first name, last name, nickname,
+login, postal address, email address, phone number, birthday date, favorite
+meal, underwear color and darkest secret*/
