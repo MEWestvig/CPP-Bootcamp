467b26ddc1ff62ac90943407fb8d5e7ccf769136
Meghan WESTVIG <mwestvig@c2r5s5.wethinkcode.co.za>
2019-06-05 11:13:23 +0200

    Added the options in the main for adding a contact

--- d00/ex01/main.cpp
+++ d00/ex01/main.cpp
@@ -6,13 +6,17 @@
 /*   By: mwestvig <mwestvig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 13:13:58 by mwestvig          #+#    #+#             */
-/*   Updated: 2019/06/05 10:44:39 by mwestvig         ###   ########.fr       */
+/*   Updated: 2019/06/05 11:13:14 by mwestvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.class.hpp"
+
 int main(int argc, char const *argv[]) {
 
-  string command;
+	int             contact_index;
+	Contact         phonebook[MAX_CONTACTS];
+	std::string     command;
 
   while (1)
   {
@@ -20,7 +24,15 @@ int main(int argc, char const *argv[]) {
     std::cin >> command;
     if (command == "ADD")
     {
-
+		if (contact_index < MAX_CONTACTS)
+		{
+			phonebook[contact_index] = addContact();
+		}
+		else
+		{
+			std::cout << "Phonebook has reached the max number of contacts."
+			std::cout << std::endl;
+		}
     }
     else if (command == "SEARCH")
     {
